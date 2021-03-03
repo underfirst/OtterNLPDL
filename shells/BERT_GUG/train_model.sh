531202fced94c29fc5d98145696a326a13c9e437
reiven <seventhtrigger0@gmail.com>
2021-03-03 22:30:26 +0900

    init commit

--- shells/BERT_GUG/train_model.sh
+++ shells/BERT_GUG/train_model.sh
@@ -1,1 +1,4 @@
-python src/controllers/BERT_GUG/train_model.py --gpus=0 --experiment_name="BERT_GUG" --server_name="hanuman"
+pipenv run python src/controllers/BERT_GUG/train_model.py \
+  --gpus=0 \
+  --experiment_name="BERT_GUG" --server_name="hanuman"
+# pipenvのshellのなかで python src/controllers/BERT_GUG/train_model.py ...でも良い
